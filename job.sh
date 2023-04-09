b7bbe05d96bedf78e4b2db60067df2dfe26f5966
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2023-04-10 02:27:02 +0900

    -V

--- job.sh
+++ job.sh
@@ -5,5 +5,5 @@ set -e
 /usr/bin/curl -s https://github.com/nostr-protocol/nips/commits/master.atom | \
     /go/bin/feed2json | \
     /usr/bin/jq -c '.items[]' | \
-    /go/bin/ocinosql-dedup -k id -hashkey | \
+    /go/bin/ocinosql-dedup -V -k id -hashkey | \
     /go/bin/jsonargs -f /go/bin/algia -V n "{{.title}} #nips_changes" "{{.url}}"
